e2c5d69158a91719e717bfeecae1bf765138df91
Marwan Mashaly <mmash102@uottawa.ca>
2021-11-09 08:42:35 -0500

    added page transition animation

--- future.js
+++ future.js
@@ -1,39 +1,22 @@
-Barba.Pjax.start();
+window.onload = () => {
+  const transition_el = document.querySelector('.transition');
+  const anchors = document.querySelectorAll('a');
 
+  setTimeout(() => {
+    transition_el.classList.remove('is-active');
+  }, 500);
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
 
-var FadeTransition = Barba.BaseTransition.extend({
-        start: function() {
-        /**
-* This function is automatically called as soon the Transition starts
-* this.newContainerLoading is a Promise for the loading of the new container
-* (Barba.js also comes with an handy Promise polyfill!)
-*/
-        // As soon the loading is finished and the old page is faded out, let's fade the new page
-        Promise
-        .all([this.newContainerLoading, this.fadeOut()])
-        .then(this.fadeIn.bind(this));
-        },
-        fadeOut: function() {
-            this.oldContainer
-        },
-        fadeIn: function() {
-            this.newContainer.classList.add('slide-in');
+    anchor.addEventListener('click', (e) => {
+      e.preventDefault();
+      let target = anchor.getAttribute('href');
+      
+      transition_el.classList.add('is-active');
 
-            var that = this;
-
-            that.newContainer.addEventListener('animationend', function() {
-                this.newContainer.classList.remove('slide-in');
-                this.done();
-            });
-        }
-});
-/**
-* Next step, you have to tell Barba to use the new Transition
-*/
-Barba.Pjax.getTransition = function() {
-        /**
-* Here you can use your own logic!
-* For example you can use different Transition based on the current page or link...
-*/
-        return FadeTransition;
+      setTimeout(() => {
+        window.location.href = target;
+      }, 500);
+  });
+  }
 };

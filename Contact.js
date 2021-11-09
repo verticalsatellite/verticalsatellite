338b87a0443e4003ddaa599fab907adaa0fcf2fc
Mykola Forest-Chomyn <mykolafc775@gmail.com>
2021-11-09 11:34:20 -0500

    Removed reload feature to save text data in Contact page

--- Contact.js
+++ Contact.js
@@ -3,9 +3,3 @@ function sendMail() {
     open("mailto:mfore048@uOttawa.ca?subject=Vertical%20Satellite%20Inquiry&body="+emailBody);
     document.getElementById("email").value = ""
 }
-
-function reload() {
-    document.getElementById("email").value = ""
-}
-
-window.onload = reload();
